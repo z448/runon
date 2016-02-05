23f716020f9cc3e35aff21f3ba32f959cd8844bf
z448 <zed448@icloud.com>
2016-02-05 10:40:03 +0100

    -

--- parallel-cmd.pl
+++ parallel-cmd.pl
@@ -7,12 +7,17 @@ use Term::ANSIColor;
 my @hosts = qw( root@10.0.0.33 z8@10.0.0.36 z@10.0.0.35 );
 my $module = $ARGV[0];
 
-my $cmd = q!export PATH="/usr/local/bin:$PATH" && mkdir -p _build_/!.qq!$module!.q! && cpanm -L _build_/!.qq!$module!.qq! $module!;
-#my $cmd = q!echo 'PATH is '$PATH && echo 'HOME is '$HOME!;
+# needs rewrite; $cmd shoud be @cmd w few default exports etc
+#my $cmd = qq!source .cypm && export PATH=/usr/local/bin:\$PATH && cpan $module!;
+
+my $cmd = q!source ./.cypm && export PATH="/usr/local/bin:$PATH" && mkdir -p _build_/!.qq!$module!.q! && cpanm -L _build_/!.qq!$module!.qq! $module!;
+
+# parallel test; uncoment below and tail /tmp/out-$host.txt on local host
+#my $cmd = 'for i in `seq 1 20`;do echo $i;sleep 3;done';
+
+# say info where is command running; put command string into separate line
 say "\nrunning:"; print colored( "$cmd\n", 'blue' ); say "on: @hosts";
 
-
-#my $cmd = 'for i in `seq 1 20`;do echo $i;sleep 3;done';
 
 my %conn = map { $_ => Net::OpenSSH->new($_, async => 1) } @hosts;
 my @pid;
